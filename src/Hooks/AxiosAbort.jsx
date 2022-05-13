5bb6cccf5fb6c1eb6ed8e200970cb3ae0131413d
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-05-13 15:21:08 +0700

    feat: Abort controller

--- src/Hooks/AxiosAbort.jsx
+++ src/Hooks/AxiosAbort.jsx
@@ -1,10 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import { useRef, useEffect } from 'react'
 
 import axios from 'axios'
 
 const useAxiosAbort = () => {
+	const Controller = useRef(new AbortController())
+
+	useEffect(() => {
+		return () => {
+			Controller.current.abort()
+		}
+	}, [])
+
 	const get = (url, options) => {
-		return axios.get(url, options)
+		return axios.get(url, { signal: Controller.current.signal, ...options })
 	}
 
 	const request = { get }
